dad8840b902a96fc06b8eeff73114356ac512e6b
ocjojo <l.ehnle@web.de>
2012-12-21 00:56:01 +0100

    integrated alex first implementations

--- WHAT/app/what/parser/ParserMediator.java
+++ WHAT/app/what/parser/ParserMediator.java
@@ -2,4 +2,19 @@ package what.parser;
 
 class ParserMediator {
 
+	private Logfile usedFile = null;
+	
+	
+	public void test() {
+		System.out.println("hallo");
+	}
+	
+	public boolean parseLogFile(String path) {
+		
+		usedFile = new Logfile(path);
+		usedFile.setPm(this);
+		System.out.println(usedFile.readLine());
+		return false;
+	}
+	
 }
