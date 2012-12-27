a1ae88c81779cc501603ab32c39b26a924c0f30b
Anas2001 <anastal2001@yahoo.com>
2012-12-27 03:50:57 -0800

    Create User.java
    To login for admin

---
+++ WHAT/app/models/User.java
@@ -0,0 +1,17 @@
+package models;
+
+/*
+ * This class for admin to upload the log file of Skyserver */ 
+
+
+Class User {
+   
+   private String id;
+   
+   private String password;
+   
+   public User (String id, String password) {
+
+   }
+
+}
