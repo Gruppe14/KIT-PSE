53085f775da627c83f99e9b2babe4de0eb011b6f
ocjojo <l.ehnle@web.de>
2013-01-16 19:06:06 +0100

    added test functionality, fixed .project file
    read comments in controllers.Test.java for instructions on testing your
    code with play.
    importing the project to eclipse should now work again

---
+++ app/controllers/Test.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.mvc.*;
+ 
+// Class for test purposes only
+public class Test extends Controller {
+	public static Result test() {
+		/**
+		 * add your methods to test here !!
+		 * if you run play, any information printed (System.out.println, etc)
+		 * will show up on the commandLine where you started the play server
+		 */
+		//go to localhost:9000/TEST to run the methods added here
+		/**
+		 * please don't upload changes made here to git,
+		 * so all members can use this file for their own purposes 
+		 */
+		
+		
+		return ok("Please look on your console for output of your code");
+	}
+}
