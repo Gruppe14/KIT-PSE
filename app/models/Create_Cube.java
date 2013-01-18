12595b00aa258441bfd3287862c8cc0b06bfc01e
Anas2001 <anastal2001@yahoo.com>
2013-01-18 13:30:44 +0100

    Some class for OLAP Cube

---
+++ app/models/Create_Cube.java
@@ -0,0 +1,12 @@
+package oracle.olapi.test;
+
+public interface Create_Cube {
+	
+	public void createAndMapCube();
+	
+	public void createAndMapMeasures();
+	
+	public void commit();
+	
+
+}
---
+++ app/models/Create_Dim.java
@@ -0,0 +1,16 @@
+package oracle.olapi.test;
+
+
+public interface Create_Dim {
+	
+	public void createDim();
+	
+	public void createAndMapDimensionLevels();
+	
+	public void createAndMapHierarchies();
+	
+	public void createAtributte();
+	
+	public void commit();
+
+}
