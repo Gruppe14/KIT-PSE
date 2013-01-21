4ca41f9a8115e62b137be0db9e3c1c70879f725b
daydreamt <d@daydreamt.net>
2013-01-21 23:57:17 +0100

    Somehow migrate charts. Split up in files. Notice common code

---
+++ public/charts/barchart/barchart.js
@@ -0,0 +1,94 @@
+function barchart(pathtodata) {
+    var data;
+    var xAxisName;
+    var yAxisName;
+    d3.json(pathtodata, function (json) {
+        console.log("I read " + json.data.length + " data points.");
+        xAxisName = json.attribute1;
+        yAxisName = json.attribute2;
+        data = json.data;
+        visualize(data); //then start the visualization
+
+    });
+
+    function visualize(data) {
+        //dimensions
+        var padding = 40;
+        var margin = {
+            top: 30,
+            right: 30,
+            bottom: 30,
+            left: 50
+        };
+        var w = 700 - margin.left - margin.right;
+        var h = 400 - margin.top - margin.bottom;
+
+        function getX(d) {
+            return d[xAxisName];
+        }
+
+        function getY(d) {
+            return d[yAxisName];
+        }
+
+        //the svg
+        var svg = d3.select("#container")
+            .append("svg")
+            .attr("class", "chart")
+            .attr("width", w + margin.left + margin.right)
+            .attr("height", h + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        //the scales
+        var xScale = d3.scale.ordinal()
+            .domain(d3.range(data.length))
+            .rangeRoundBands([0, w], 0.04);
+
+
+
+        var yScale = d3.scale.linear()
+            .domain([0, d3.max(data, getY)])
+            .range([h, 0]);
+
+        //the axes
+        var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
+        var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(2);
+
+
+
+
+        //add the data and bars
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", function (d, i) {
+            return xScale(i);
+        })
+            .attr("y", function (d) {
+            return h - yScale(getY);
+        })
+            .attr("width", xScale.rangeBand())
+            .attr("height", function (d) {
+            return yScale(getY);
+        })
+            .attr("class", "bar");
+
+        //create axes
+
+        svg.append("g").attr("class", "x axis")
+            .attr("transform", "translate(0," + h + ")").call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(xAxisName);
+            
+    }
+}
---
+++ public/charts/scatterplot/scatterplot.js
@@ -0,0 +1,103 @@
+//read the data, store them in global variables
+function scatterplot(pathtodata) {
+    var data;
+    var xAxisName;
+    var yAxisName;
+    d3.json(pathtodata, function (json) {
+        console.log("I read " + json.data.length + " data points.");
+        xAxisName = json.attribute1;
+        yAxisName = json.attribute2;
+        data = json.data;
+        visualize(data); //then start the visualization
+
+    });
+
+    function visualize(data) {
+        //dimensions of the chart
+        var w = 720;
+        var h = 640;
+        //empty area between last element and chart borders
+        var padding = 20;
+
+        //the format of the data
+        var format = d3.format(".0");
+
+        //will use this one below
+        function getX(d) {
+            return d[xAxisName];
+        };
+
+        function getY(d) {
+            return d[yAxisName];
+        };
+
+        //the scales
+        var xScale = d3.scale.linear()
+            .domain([d3.min(data, getX), d3.max(data, getX)])
+            .range([padding, w - padding]);
+        var yScale = d3.scale.linear()
+            .domain([d3.min(data, getY), d3.max(data, getY)])
+            .range([h - padding, padding]);
+
+        //the axes
+        var xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom")
+            .ticks(3)
+            .tickFormat(format);
+
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left")
+            .ticks(3)
+            .tickFormat(format);
+
+
+
+        //the svg chart!
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+
+
+        //create the points of the scatterplot
+        //well, they are svg circles
+        svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+            return xScale(d[xAxisName]);
+        })
+            .attr("cy", function (d) {
+            return yScale(d[yAxisName]);
+        })
+            .attr("r", 2) //arbitrary. big enough? scale automatically?
+        .attr("x", getX)
+            .attr("x", getY)
+            .attr("class", "circle")
+            .append("svg:title")
+            .text(function (d) {
+            return "(" + d.x + "," + d.y + ")";
+        });
+
+
+        //create the axes, too
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (h - padding) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding + ",0)")
+            .call(yAxis);
+
+
+    }
+
+
+
+}
