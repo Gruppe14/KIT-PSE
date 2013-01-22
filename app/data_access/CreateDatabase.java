4b1c652c652c041c39f45fdbe71c981580f65a25
Anas2001 <anastal2001@yahoo.com>
2013-01-22 21:31:01 +0100

    Update app/data_access/CreateDatabase.java
    ich bin fast fertig damit ;)

--- app/data_access/CreateDatabase.java
+++ app/data_access/CreateDatabase.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 
 public class CreateDatabase {
 	
-	private String Username;
+    private String Username;
     private String Password;
     private String Driver = "com.mysql.jdbc.Driver";
     private String URL;
@@ -22,75 +22,77 @@ public class CreateDatabase {
     private Connection conn;
     
     
-    public CreateDatabase(String username, String password) {
+    public CreateDatabase(String newName, String username, String password) {
     	
     	this.Username = username;
     	this.Password = password;
+    	this.newName  = newName;
     	this.URL = "jdbc:mysql://localhost:3306/olapwhat";
-    	
     	this.makeConnection();
+    	this.createDatabaseName();
+    	this.createTimeTabele();
+    	this.createDBTabele();
+    	this.createLocationTabele ();
+    	this.createQueryEntryTabele ();
+    	this.closeConnection();
     	
     }
     
     private boolean makeConnection() {
 		
     	try {
-            Class.forName(this.Driver);
-            this.conn = DriverManager.getConnection(this.URL,
-                    this.Username, this.Password);
-            return true;
-        } catch (Exception e) {
-           System.out.println("No Connection are allowd!");
-           return false;
-        }
+                 Class.forName(this.Driver);
+                 this.conn = DriverManager.getConnection(this.URL,
+                           this.Username, this.Password);
+                 return true;
+                 
+            } catch (Exception e) {
+                  System.out.println("No Connection are allowd!");
+                  return false;
+            }
 		
-	}
+    }
     
-    public void createDatabaseName(String name) {
+    private void createDatabaseName() {
     	
        try {
 				    
-    	    Statement stmt = (Statement) this.conn.createStatement();
+    	                Statement stmt = (Statement) this.conn.createStatement();
 			
-			String query = "CREATE DATABASE " + name;
+			String query = "CREATE DATABASE " + this.newName;
 
 			stmt.executeUpdate(query);
 			      
-			      this.newName = name;
-		    	  this.newDBUrlName();
 			      this.closeConnection();
+			      this.URL = "jdbc:mysql://localhost:3306/" + this.newName;
 			      this.makeConnection();
+			      
+			      return false;
 			
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
-			System.out.println("iam hier");
+			return false;
 			
 		}
     	
     }
     
-    private void newDBUrlName() {
-    	
-    	this.URL = "jdbc:mysql://localhost:3306/" + this.newName;
-    }
     
-    public void createTimeTabele() {
+    private void createTimeTabele() {
     	
     	try {
     		
     		Statement stmt = (Statement) this.conn.createStatement();
     		
     		String table = 
-    				       " create table `time_dim` ( " +
+    				       " CREATE TABLE IF NOT EXISTS  `time_dim` ( " +
     				       " `time_id` int (11), " +
     				       " `year` year (4), " +
     				       " `month` int (11), " +
     				       " `day` int (11), " +
     				       " `hour` int (11), " +
-    				       " `elapse time` int (11), " +
-    				       " `cpu time` int (11), " +
-    				       " `rows time` int (11) " +
-    				       " );" ;
+    				       " `min` int (11), " +
+    				       " `sec` int (11) );" ;
     		
     		stmt.execute(table);
     		
@@ -100,42 +102,17 @@ public class CreateDatabase {
     	
     }
     
-    public void createDBTabele() {
+    private void createDBTabele() {
     	
       try {
     		
     		Statement stmt = (Statement) this.conn.createStatement();
     		
     		String table = 
-    				       " create table IF NOT EXISTS `db_dim` ( " +
+    				       " CREATE TABLE IF NOT EXISTS  `db_dim` ( " +
     				       " `db_i` int (11), " +
-    				       " `database` varchar (765), " +
-    				       " `elapse time` int (11), " +
-    				       " `cpu time` int (11), " +
-    				       " `rows` int (11));" ;
-    		
-    		stmt.execute(table);
-    		
-    	} catch(SQLException e3) {
-    		
-    	}
-    	
-    }
-    
-    public void createServerTabele () {
-    	
-       try {
-    		
-    		Statement stmt = (Statement) this.conn.createStatement();
-    		
-    		String table = 
-    				       " create table IF NOT EXISTS `server_dim` ( " +
-    				       " `server_id` int (11), " +
-    				       " `type` varchar (765), " +
-    				       " `elapse time` int (11), " +
-    				       " `cpu time` int (11), " +
-    				       " `rows` int (11)" +
-    				       ");";
+    				       " `databasename` varchar (765), " +
+    				       " `servername` varchar (765) )";
     		
     		stmt.execute(table);
     		
@@ -146,20 +123,18 @@ public class CreateDatabase {
     }
     
     
-    public void createLocationTabele () {
+    
+    private void createLocationTabele () {
     	
      try {
     		
     		Statement stmt = (Statement) this.conn.createStatement();
     		
     		String table = 
-    				       " CREATE TABLE  location_dim ( " +
+    				       " CREATE TABLE IF NOT EXISTS location_dim ( " +
     				       " `location_id` int (11), " +
     				       " `country` varchar (765), " +
     				       " `city` varchar (765), " +
-    				       " `elapse time` int (11), " +
-    				       " `cpu time` int (11), " +
-    				       " `rows` int (11)" +
     				       ");" ;
     		
     		stmt.execute(table);
@@ -169,7 +144,7 @@ public class CreateDatabase {
     	}
     }
     
-    public void createQueryEntryTabele () {
+    private void createQueryEntryTabele () {
     	
         try {
        		
@@ -181,6 +156,9 @@ public class CreateDatabase {
        				       " location_id int (11), " +
        				       " db_id int (11), " +
        				       " server_id int (11) " +
+       				       " `elapse time` int (11), " +
+    				       " `cpu time` int (11), " +
+    				       " `rows` int (11)" +
        				       ");" ;
        		
        		stmt.execute(table);
@@ -188,23 +166,23 @@ public class CreateDatabase {
        	} catch(SQLException e3) {
        		
        	}
-       }
+     }
     
-    public void closeConnection() {
+    private void closeConnection() {
 		
 		if (this.conn != null) {
             try {
                 this.conn.close();
             } catch (Exception e) {
             }
-        }
+    }
 		
-	}
+    }
     
     
    /* 
     public static void main(String[] args) {
-    	CreateDatabase g = new CreateDatabase("root", "password");
+    	
     	
 		
 	   } */
