e2553f6eb9596dd3abc2a2da25eef67c063b0e60
daydreamt <d@daydreamt.net>
2013-02-26 01:18:28 +0100

    Why does travis complain?

--- conf/dbinit.sql
+++ conf/dbinit.sql
@@ -1,4 +1,8 @@
 CREATE USER 'adminWHAT'@'localhost' IDENTIFIED BY 'whatUP';
 GRANT ALL PRIVILEGES ON *.* TO 'adminWHAT'@'localhost';
 
+CREATE USER 'masteWHAT'@'localhost' IDENTIFIED BY 'whatUP';
+GRANT ALL PRIVILEGES ON *.* TO 'masteWHAT'@'localhost';
+
+
 CREATE DATABASE WHATDataWarehouse;
