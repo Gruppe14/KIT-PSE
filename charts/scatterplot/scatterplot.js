fcc9e055feaac677041ecb85f39b65d13e8c2dcf
ocjojo <l.ehnle@web.de>
2013-02-27 12:43:23 +0100

    scatterplot can use css from bubblescatter

--- charts/scatterplot/scatterplot.js
+++ charts/scatterplot/scatterplot.js
@@ -1,10 +1,12 @@
 
 function scatterplot(json ){
-    try {
-        bubblescatter(json, 3);
-    }
-    catch(err) {
-      $("head").append('<script type="text/javascript" src="/charts/bubblescatter/bubblescatter.js"></script>');
-       bubblescatter(json);
-    }
+	//load js from bubblescatter if not there
+	if($("script[src='/charts/bubblescatter/bubblescatter.js']").length < 1) {
+		$("head").append('<script type="text/javascript" src="/charts/bubblescatter/bubblescatter.js"></script>');
+	}
+	//load css from bubblescatter if not there
+	if($("link[href='/charts/bubblescatter/bubblescatter.css']").length < 1){
+		$("head").append('<link href="/charts/bubblescatter/bubblescatter.css" rel="stylesheet" type="text/css">');
+	}
+    bubblescatter(json, 3);
 }
