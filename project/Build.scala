b5eb8bc895dfcc5c4c2364ec82f1b099f60af665
daydreamt <d@daydreamt.net>
2013-02-28 04:11:03 +0100

    Remove lib folder; Download dependencies from the internet.

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-		"mysql" % "mysql-connector-java" % "5.1.18"
+		"mysql" % "mysql-connector-java" % "5.1.18",
+		"junit" % "junit" % "4.10",
+		"org.json" % "json" % "20090211",
+		"org.apache.commons" % "commons-lang3" % "3.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
