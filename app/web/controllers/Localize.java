bf7bbfedca456f407ea2f6e263aefcc6a92f9fe1
ocjojo <l.ehnle@web.de>
2013-03-02 00:49:51 +0100

    fix errors while testing with localize

--- app/web/controllers/Localize.java
+++ app/web/controllers/Localize.java
@@ -8,18 +8,21 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * Static Class handling the localization of the web page and other parts of the program
+ * Singleton/static Class handling the localization of the web page and other parts of the program
  * providing mainly two methods for translation, one with fallback (get) the other without(getString).
+ * the second can be used without instantiating the singleton class, it then returns the standard language.
  * 
  * @author Lukas Ehnle, PSE Gruppe 14
  *
  */
 public class Localize extends Controller {
+	/** instance of this singleton class */
+	private static Localize instance = null;
 	/**
 	 * a list containing all available languages,
 	 * meaning a language file (e.g. messages_en.properties) was found
 	 */
-	private static List<Lang> available = Lang.availables();
+	private static List<Lang> available;
 	/**
 	 * the default language, which is returned by the fallback mechanism,
 	 * if the requested language doesn't exist.
@@ -36,6 +39,7 @@ public class Localize extends Controller {
 	 * private constructor because of singleton pattern used.
 	 */
 	private Localize() {
+		available = Lang.availables();
 	}
 	
 
@@ -46,7 +50,7 @@ public class Localize extends Controller {
 	 * the string in the standard language and if that is also not found returns the key
 	 */
 	public static String get(String message) {
-		return get(message, language()); 
+		return Localize.instance().get(message, language()); 
 	}
 	
 	/**
@@ -77,6 +81,10 @@ public class Localize extends Controller {
 	 * else the standard language is returned.
 	 */
 	public static String language() {
+		//could be the 
+		if(instance == null) {
+			return standard;
+		}
 		String lang = session("lang");
 		if (lang == null || !available.contains(Lang.forCode(lang))) {
 	    	List<Lang> want = request().acceptLanguages();
@@ -102,7 +110,7 @@ public class Localize extends Controller {
 	 * @param lang the language to get
 	 * @return returns a localized string
 	 */
-	private static String get(String key, String lang) {
+	private String get(String key, String lang) {
 		try {
 			return ResourceBundle.getBundle("messages_" + lang, control).getString(key);
 			//default language if message not found
@@ -113,5 +121,15 @@ public class Localize extends Controller {
 				return key;
 			}
 		}
+	}
+	/**
+	 * instantiates this class if not already done and returns this instance.
+	 * @return returns a Localize object.
+	 */
+	private static Localize instance() {
+		if(Localize.instance == null) {
+			Localize.instance = new Localize();
+		}
+		return Localize.instance;
 	}
 }
