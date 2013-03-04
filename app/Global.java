38f2f24f8c74d5a1d4af66d6977e85adcd9fa1be
Jo Klawitter <jo.klawitter@gmail.com>
2013-03-04 02:33:25 +0100

    not web page start, too bad

--- app/Global.java
+++ app/Global.java
@@ -8,16 +8,10 @@ import what.Facade;
 import what.Printer;
 
 public class Global extends GlobalSettings {
-	
+		
 	public void onStart(Application app) {
 		Printer.print("Welcome to WHAT!");
-		try {
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:9000/"));
-		} catch (IOException e) {
-			Printer.pfail("IO while starting web page.");
-		} catch (URISyntaxException e) {
-			Printer.pfail("Creating URI while starting web page.");
-		}
+		
 	}
 	
 	public void onStop(Application app) {
