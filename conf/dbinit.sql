2da4ab595a9b6b9acdb0a6670a338ace626118ee
ocjojo <l.ehnle@web.de>
2013-03-08 18:48:43 +0100

    again..

--- conf/dbinit.sql
+++ conf/dbinit.sql
@@ -1,8 +1,5 @@
-CREATE USER 'adminWHAT'@'localhost' IDENTIFIED BY 'whatUP';
-GRANT ALL PRIVILEGES ON *.* TO 'adminWHAT'@'localhost';
-
 CREATE USER 'masteWHAT'@'localhost' IDENTIFIED BY 'whatUP';
 GRANT ALL PRIVILEGES ON *.* TO 'masteWHAT'@'localhost';
 
 
-CREATE DATABASE WHATDataWarehouse;
+CREATE DATABASE whatdatawarehouse;
